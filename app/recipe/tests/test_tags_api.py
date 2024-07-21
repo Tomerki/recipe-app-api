797754478f183dfa4ed5bfe68c477e90addb274f
Tomerki <Tomerh1810@gmail.com>
2024-07-21 14:10:55 +0300

    Implement tag listing API (Tag serializer)

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -50,14 +50,14 @@ class PrivateTagsApiTests(TestCase):
         tags = Tag.objects.all().order_by('-name')
         serializer = TagSerializer(tags, many=True)
 
-        self.assertEqual(res.status_code, status_HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
         """Test list of tags is limited to authenticated user."""
         user2 = create_user(email='user2@example.com')
         Tag.objects.create(user=user2, name='åFruity')
-        tag = Tags.objects.create(user=self.user, name='Comfort food')
+        tag = Tag.objects.create(user=self.user, name='Comfort food')
 
         res = self.client.get(TAGS_URL)
 
