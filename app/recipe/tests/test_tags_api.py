901f8cafedeca0659bb46eaab4ebd0e45af73fce
Tomerki <Tomerh1810@gmail.com>
2024-07-21 14:31:32 +0300

    Implement delete tag API.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -93,6 +93,6 @@ class PrivateTagsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         tags = Tag.objects.filter(user=self.user)
-        self.assertFlase(tags.exists())
+        self.assertFalse(tags.exists())
 
 
